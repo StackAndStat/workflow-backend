277ee161132dec94ceeedad209909becb5ad4d01
StackAndStat <StackAndStat@gmail.com>
2025-11-17 08:38:34 +0100

    Third commit

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/register", registerRouter);
