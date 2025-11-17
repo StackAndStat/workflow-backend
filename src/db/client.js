0dc306b55fb4eb6491437b74737b980d54edbfa3
StackAndStat <StackAndStat@gmail.com>
2025-11-17 10:19:17 +0100

    Fourth commit

--- src/db/client.js
+++ src/db/client.js
@@ -5,20 +5,12 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const databaseConfig = process.env.DATABASE_URL
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {
-      user: process.env.PG_USER,
-      host: process.env.PG_HOST,
-      database: process.env.PG_DATABASE,
-      password: process.env.PG_PASSWORD,
-      port: parseInt(process.env.PG_PORT),
-    };
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 const db = new Pool(databaseConfig);
 
